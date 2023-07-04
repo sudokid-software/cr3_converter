965fea083d41b6c0e76efccb98d8b34d4ea8c1f0
ethan <code@gweithio.com>
2023-07-04 23:28:34 +0100

    re-structure

---
+++ include/converter/Manifest.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <converter/ImageData.hpp>
+
+#include <string>
+#include <vector>
+
+class Manifest final {
+public:
+  Manifest() = delete;
+  explicit Manifest(std::string &filepath,
+                    std::vector<ImageData> data) noexcept;
+
+  [[nodiscard]] bool write();
+  [[nodiscard]] bool thumbnail(LibRaw &ImageProcessor, std::string &output_file,
+                               int thumbnail_index);
+
+  ~Manifest();
+
+private:
+  std::string _manifest_file_path;
+  std::vector<ImageData> _image_data;
+};
