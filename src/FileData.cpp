cb05f429e5d1ef8a7b0b76ddf7c6cd4c557b9592
ethan <code@gweithio.com>
2023-07-05 00:36:40 +0100

    restructure

---
+++ src/FileData.cpp
@@ -0,0 +1,15 @@
+#include <converter/FileData.hpp>
+
+#include <sstream>
+
+FileData::FileData(std::string name, int width, int height) noexcept
+    : _name(std::move(name)), _width(width), _height(height) {}
+
+[[nodiscard]] std::string FileData::get_json() const {
+  std::ostringstream json;
+  json << R"({"fileName": ")" << _name << R"(","width":)" << _width
+       << R"(,"height":)" << _height << "}";
+  return json.str();
+}
+
+FileData::~FileData(){};
